6e9258e4fc6a23ba6dfafce01d116a4d237fa28f
geonunez <jorge.nunez@geonunez.com>
2017-08-28 18:41:04 +0000

    Completed exercite number one

---
+++ 1/README.md
@@ -0,0 +1,5 @@
+#### Escribe un script que a partir de un array de ints devuelva un array de strings aplicando las siguientes reglas:
+
+* Devuelve Fizz si el número es divisible por 3 o si incluye un 3 en el número.
+* Devuelve Buzz si el número es divisible por 5 o si incluye un 5 en el número.
+* Devuelve FizzBuzz si el número es divisible por 3 y por 5.
---
+++ 1/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * Verifies if the number is divisible by a divider
+ * or the number contains the divider.
+ * 
+ * @param int number 
+ * @param int divider 
+ */
+function isDivisibleOrContains(number, divider) {
+    return (number % divider === 0 
+        || number.toString().indexOf(divider) !== -1 );
+}
+
+(() => {
+    var numbers = [1,3,4,5,6,8,9,10,12,15,16,18,20,21,24,25,27,28,30,32,33,35,36,39,40,42,44,45,48,50];
+    
+    var strings = numbers.map((number) => {
+        var string = "";
+        
+        if (isDivisibleOrContains(number, 3)) {
+            string += "fuzz";
+        }
+    
+        if (isDivisibleOrContains(number, 5)) {
+            string += "buzz";
+        }
+        
+        return string;
+    });
+
+    console.log(strings);
+})();
