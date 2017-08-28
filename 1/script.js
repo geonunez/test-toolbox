14928862fd467a8253de1b3eb399096d27eed4ab
geonunez <jorge.nunez@geonunez.com>
2017-08-28 19:14:32 +0000

    Fixed a little bug

--- 1/script.js
+++ 1/script.js
@@ -12,22 +12,36 @@ function isDivisibleOrContains(number, divider) {
         || number.toString().indexOf(divider) !== -1 );
 }
 
-(() => {
-    var numbers = [1,3,4,5,6,8,9,10,12,15,16,18,20,21,24,25,27,28,30,32,33,35,36,39,40,42,44,45,48,50];
-    
+/**
+ * Transforms an array of numbers to an array of strings
+ * following some rules.
+ * 
+ * @param array numbers 
+ */
+function numbersToStrings(numbers) {
     var strings = numbers.map((number) => {
         var string = "";
-        
+
         if (isDivisibleOrContains(number, 3)) {
-            string += "fuzz";
+            string += "Fizz";
         }
     
         if (isDivisibleOrContains(number, 5)) {
-            string += "buzz";
+            string += "Buzz";
         }
         
         return string;
     });
+
+    return strings;
+}
+/**
+ * Main
+ */
+(() => {
+    var numbers = [1,3,4,5,6,8,9,10,12,15,16,18,20,21,24,25,27,28,30,32,33,35,36,39,40,42,44,45,48,50];
+    
+    var strings = numbersToStrings(numbers);    
 
     console.log(strings);
 })();
